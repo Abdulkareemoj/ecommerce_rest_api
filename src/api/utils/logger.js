157365104ab2b696eb2ec4abf16612769738eb65
beauspot <beauspot@outlook.com>
2023-06-24 21:16:39 +0100

    finished setting up the custom logger module for logging purposes on the console.

---
+++ src/api/utils/logger.js
@@ -0,0 +1,17 @@
+// Transports are places where you want to save your logs
+
+import expressWinston from "express-winston";
+
+import { consoleLogger } from "./componentLogger.js";
+import { customErrorLogger } from "./errCustomLogger.js";
+
+export const customLogger = expressWinston.logger({
+  winstonInstance: consoleLogger,
+  statusLevels: true,
+});
+
+// custom error handler logger
+export const errorCustomLogger = expressWinston.errorLogger({
+  winstonInstance: customErrorLogger,
+  statusLevels: true,
+});
