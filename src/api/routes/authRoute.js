4601008e4b11a4779ffc5a9813bb176a175019ae
beauspot <beauspot@outlook.com>
2023-06-27 09:10:03 +0100

    created the routes for login routes for the users.

--- src/api/routes/authRoute.js
+++ src/api/routes/authRoute.js
@@ -1,8 +1,9 @@
 import express from "express";
-import {create_a_user} from "../controllers/userCtrls.js";
+import { create_a_user, LoginUser } from "../controllers/userCtrls.js";
 
 const authRoute = express.Router();
 
 authRoute.post("/signup", create_a_user);
+authRoute.post("/login", LoginUser);
 
 export default authRoute;
