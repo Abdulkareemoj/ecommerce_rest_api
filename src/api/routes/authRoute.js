2f8bafd0c7527bb1de4310e0d26cb1634149db05
beauspot <beauspot@outlook.com>
2023-06-27 10:54:09 +0100

    finishsed setting up or linking up the controler for getting all users.

--- src/api/routes/authRoute.js
+++ src/api/routes/authRoute.js
@@ -1,9 +1,14 @@
 import express from "express";
-import { create_a_user, LoginUser } from "../controllers/userCtrls.js";
+import {
+  create_a_user,
+  LoginUser,
+  getAllUser,
+} from "../controllers/userCtrls.js";
 
 const authRoute = express.Router();
 
 authRoute.post("/signup", create_a_user);
 authRoute.post("/login", LoginUser);
+authRoute.get("/allusers", getAllUser);
 
 export default authRoute;
