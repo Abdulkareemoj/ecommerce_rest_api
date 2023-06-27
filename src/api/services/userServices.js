7fb46fce5f830363af5baaef3ebbf9e746e36cf1
beauspot <beauspot@outlook.com>
2023-06-27 09:09:18 +0100

    Created the user service module for the user authentication

--- src/api/services/userServices.js
+++ src/api/services/userServices.js
@@ -13,3 +13,29 @@ export const create_user_service = async (userData) => {
   mailer(email, subject, text);
   return { newUser, userToken };
 };
+
+export const login_user_service = async (userData) => {
+  const { email, password } = userData; // Extract Email and Password fro userData
+  const userExists = await authModel.findOne({ email: email });
+  if (!userExists) {
+    throw new Error("Password or email didn't match any on our database");
+  }
+  const isMatch = await userExists.comparePwd(password);
+  if (!isMatch) {
+    throw new Error("Password or email didn't match any on our database");
+  }
+  const token = userExists.createJWT();
+  return { userExists, token };
+};
+
+/**
+ *  const userExists = await authModel.findOne({ email: userData });
+  const isMatch = await userExists.comparePwd(userData.password);
+  if (!isMatch) {
+    throw new Error("Password or email didn't match any on our database");
+  } else {
+    const token = userExists.createJWT();
+    console.log(token);
+    return { userExists, token };
+  }
+ */
