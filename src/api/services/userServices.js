be3cfeaa187b139a9841860e6d51c2c1e9b99f9a
beauspot <beauspot@outlook.com>
2023-06-28 20:27:25 +0100

    Implemented features for the user services.

--- src/api/services/userServices.js
+++ src/api/services/userServices.js
@@ -39,10 +39,33 @@ export const get_all_users_service = async (users) => {
 // Get a Single user Service
 export const get_single_user_service = async (userID) => {
   const { id } = userID; // destructure the user ID from the user
-  const userExists = await authModel.findOne({ UserId: id });
+  const userExists = await authModel.findById({ _id: id });
   console.log(userExists);
   if (!userExists) {
     throw new Error(`The User with the ID ${id} does not exist`);
   }
   return userExists;
+};
+
+//Delete a single user service
+export const delete_single_user = async (userId) => {
+  const { id } = userId;
+  const user = await authModel.findOneAndDelete({ _id: id });
+  console.log(user);
+  if (!user) throw new Error("The user was not found.");
+  return user;
+};
+
+// Updating the user Service
+export const updateUserService = async (userId, updateData) => {
+  const { id } = userId;
+  const updateuser = await authModel.findOneAndUpdate({ _id: id }, updateData, {
+    new: true,
+    runValidators: true,
+  });
+  console.log(userId);
+  if (!updateuser) {
+    throw new Error("The user was not found to be updated");
+  }
+  return updateuser;
 };
