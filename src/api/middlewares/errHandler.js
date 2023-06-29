7f21f256495de69d91ed4d9009c1a37766a3dd0e
beauspot <beauspot@outlook.com>
2023-06-29 08:03:03 +0100

    Corrected the mesage property to message property.

--- src/api/middlewares/errHandler.js
+++ src/api/middlewares/errHandler.js
@@ -1,7 +1,7 @@
 import CustomAPIError from "../helpers/custom-errors.js";
 const errorHandlerMiddleware = (err, req, res, next) => {
   if (err instanceof CustomAPIError) {
-    return res.status(err.statusCode).json({ msg: err.mesage });
+    return res.status(err.statusCode).json({ msg: err.message });
   }
   return res.status(500).json({ msg: err.message });
 };
