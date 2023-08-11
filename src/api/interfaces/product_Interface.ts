1c5f246d8e645507ea83641fefe1a2cdf6d69c10
beauspot <beauspot@outlook.com>
2023-08-11 15:13:22 +0100

    added  GetAllProductsQueryParams interface on the directory.

--- src/api/interfaces/product_Interface.ts
+++ src/api/interfaces/product_Interface.ts
@@ -1,4 +1,5 @@
 import { Document, Types } from "mongoose";
+import { SortByOptions } from "../types/sortOptions";
 
 // Defining the interface for the rating object
 interface Rating {
@@ -21,4 +22,22 @@ export interface ProductDataInterface extends Document {
   ratings: Rating[]; // Ratings array with the Rating interface
   createdAt: Date;
   updatedAt: Date;
+}
+
+export interface GetAllProductsOptions {
+  sortBy: string | 'createdAt' | 'title' | 'price';
+  sortOrder: "asc" | "desc";
+  limit: number;
+  page: number;
+  category: string;
+  brand: string;
+}
+
+export interface GetAllProductsQueryParams {
+  sortBy?: SortByOptions;
+  sortOrder?: "asc" | "desc";
+  limit?: number;
+  page?: number;
+  category?: string;
+  brand?: string;
 }
