b98789c89d21b50c59eda8d0f5549551f3f6e006
beauspot <beauspot@outlook.com>
2023-09-05 23:58:26 +0100

    finished setting up the brand model.

---
+++ src/api/models/brands.models.ts
@@ -0,0 +1,18 @@
+import { BrandInterface } from "../interfaces/brand.interface";
+import mongoose, { Schema } from "mongoose";
+
+const brandSchema: Schema<BrandInterface> = new Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      unique: true,
+      index: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export const BrandModel = mongoose.model<BrandInterface>("Brand", brandSchema);
