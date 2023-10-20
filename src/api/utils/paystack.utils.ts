943ece11766c0bf4cf4e34093df8587e2e881f76
beauspot <beauspot@outlook.com>
2023-10-20 12:12:28 +0100

    finished setting up the paystack utility function.

---
+++ src/api/utils/paystack.utils.ts
@@ -0,0 +1,48 @@
+/* 
+    create our function that calls paystack-API and 
+    submits our transaction details and authorization 
+    headers to initialize and verify them.
+*/
+
+import dotenv from "dotenv";
+import * as request from "request";
+import { Form, Callback } from "../interfaces/paystack_init.interface";
+
+dotenv.config();
+
+export const paystack_init = (request: any) => {
+  const Secret_key = process.env.PAY_STACK_API_KEY;
+
+  const initializePayment = (form: Form, mycallBack: Callback) => {
+    const options = {
+      url: "https://api.paystack.co/transaction/initialize",
+      headers: {
+        authorization: Secret_key,
+        "content-type": "application/json",
+        "cache-control": "no-cache",
+      },
+      form,
+    };
+    const callBack: request.RequestCallback = (error, response, body) => {
+      return mycallBack(error, body);
+    };
+    request.post(options, callBack);
+  };
+
+  const verifyPayment = (ref: string, mycallBack: Callback) => {
+    const options = {
+      url:
+        "https://api.paystack.co/transaction/verify/" + encodeURIComponent(ref),
+      headers: {
+        authorization: Secret_key,
+        "content-type": "application/json",
+        "cache-control": "no-cache",
+      },
+    };
+    const callback: request.RequestCallback = (error, response, body) => {
+      return mycallBack(error, body);
+    };
+    request(options, callback);
+  };
+  return { initializePayment, verifyPayment };
+};
