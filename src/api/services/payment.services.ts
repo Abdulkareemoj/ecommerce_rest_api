c33381f3b4a2bb2f9c48eba1bc280614f58e0a69
beauspot <beauspot@outlook.com>
2023-10-26 08:10:17 +0100

    modified the payment services.

--- src/api/services/payment.services.ts
+++ src/api/services/payment.services.ts
@@ -1,77 +1,52 @@
-import * as request from "request";
 import { checkoutModel } from "../models/checkout_model";
-import loadash from "lodash";
-import { paystack_init } from "../utils/paystack.utils";
-import { Form, PaymentData } from "../interfaces/paystack_init.interface";
-
-const { initializePayment, verifyPayment } = paystack_init(request);
+import { PaymentData } from "../interfaces/paystack_init.interface";
+import { InitializePaymentInput } from "../types/payment_init";
+import { initializePayment, verifyPayment } from "../utils/paystack.utils";
 
 export class PaymentService {
-  startPayment(data: PaymentData) {
-    return new Promise(async (resolve, reject) => {
-      try {
-        const form: Form = {
-          price: data.amount * 100,
-          email: data.email,
-          firstName: data.firstName,
-          lastName: data.lastName,
-          metadata: {
-            full_name: data.firstName + " " + data.lastName,
-          },
-        };
-        initializePayment(form, (error, response: request.Response, body) => {
-          if (error) {
-            reject(error.message);
-          }
-          const body_ = Response.body;
-          const parsedBody = JSON.parse(body_);
+  async startPayment(data: PaymentData) {
+    try {
+      const paymentData: InitializePaymentInput = {
+        price: data.amount * 100,
+        email: data.email,
+        firstName: data.firstName,
+        lastName: data.lastName,
+        metadata: {
+          full_name: data.firstName + " " + data.lastName,
+        },
+      };
 
-          return resolve(JSON.stringify(parsedBody));
-        });
-      } catch (error: any) {
-        error.source = "Start Payment Service";
-        return reject(error);
-      }
-    });
-  }
-
-  createPayment(req: Request) {
-    const ref = req.reference;
-    if (ref == null) {
-      return reject({ code: 400, msg: "No Reference Passed in the query!" });
+      return await initializePayment(paymentData);
+    } catch (error) {
+      console.error;
     }
-    return new Promise(async (resolve, reject) => {
-      try {
-        verifyPayment(ref, (error, body) => {
-          if (error) {
-            reject(error.message);
-          }
-          const response = JSON.parse(body);
-
-          const { reference, amount, status } = response.data;
-          const { email } = response.data.customer;
-          const full_name = response.data.metadata.full_name;
-          const newPayment = { reference, amount, email, full_name, status };
-          const payment = checkoutModel.create(newPayment);
-
-          return resolve(payment);
-        });
-      } catch (error: any) {
-        error.source = "Create Payment Service";
-        return reject(error);
-      }
-    });
   }
-  paymentReceipt(body) {
-    return new Promise(async (resolve, reject) => {
-      try {
-        const reference = body.reference;
-        const transaction = checkoutModel.findOne({ reference: reference });
-        return resolve(transaction);
-      } catch (error: any) {
-        error.source = "Payment Receipt";
-        return reject(error);
-      }
-    });
+
+  async createPayment(reference: string) {
+    try {
+      const response = await verifyPayment(reference);
+      const data = response?.data;
+      const payment = await checkoutModel.create({
+        reference: data.reference,
+        amount: data.amount,
+        email: data.customer.email,
+        full_name: data.metadata.full_name,
+        status: data.status,
+      });
+
+      return payment;
+    } catch (error) {
+      console.error;
+    }
+  }
+
+  async paymentReceipt(reference: string) {
+    try {
+      const transaction = await checkoutModel.findOne({ reference: reference });
+      return transaction;
+    } catch (error: any) {
+      error.source = "Payment Receipt";
+      console.error;
+    }
   }
 }
